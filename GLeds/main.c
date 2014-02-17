aa11638fe82e2ae4aa93c1e22a2f329d8099900f
Jean <jean.begaint@gmail.com>
2014-02-17 11:54:08 +0100

    indent

--- GLeds/main.c
+++ GLeds/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <gtk/gtk.h> 
+#include <gtk/gtk.h>
 
 #define GLADE_FILE "Gleds.glade"
 
@@ -13,7 +13,7 @@
 #define N_ROWS 14
 #define N_COLS 25
 
-char *LED_ARRAY[3] = {LED0, LED1, LED2}; 
+char *LED_ARRAY[3] = { LED0, LED1, LED2 };
 
 typedef struct _data {
 	GtkWidget *start_button;
@@ -27,20 +27,20 @@ void quit(void)
 	gtk_main_quit();
 }
 
-void on_open_activate(GtkWidget *widget, gpointer user_data)
+void on_open_activate(GtkWidget * widget, gpointer user_data)
 {
 	g_print("%s clicked!\n", gtk_widget_get_name(widget));
 }
 
-void on_pause_clicked(GtkWidget *widget, gpointer user_data)
+void on_pause_clicked(GtkWidget * widget, gpointer user_data)
 {
 	GtkWidget *launch_button = NULL;
 	g_print("%s clicked!\n", gtk_widget_get_name(widget));
 }
 
-void on_launch_clicked(GtkWidget *widget, gpointer user_data)
+void on_launch_clicked(GtkWidget * widget, gpointer user_data)
 {
-	/*gtk_widget_set_sensitive(widget, FALSE);*/
+	/*gtk_widget_set_sensitive(widget, FALSE); */
 
 	GList *children, *l;
 	GtkWidget *grid = NULL;
@@ -57,9 +57,8 @@ void on_launch_clicked(GtkWidget *widget, gpointer user_data)
 		if (!led_state) {
 			gtk_image_set_from_file(GTK_IMAGE(image), LED0);
 			continue;
-		}
-		else {
-			gtk_image_set_from_file(GTK_IMAGE(image), LED_ARRAY[rand()%3]);
+		} else {
+			gtk_image_set_from_file(GTK_IMAGE(image), LED_ARRAY[rand() % 3]);
 		}
 	}
 
@@ -67,64 +66,64 @@ void on_launch_clicked(GtkWidget *widget, gpointer user_data)
 	g_free(led_state);
 }
 
-void on_window_destroy(GtkWidget *widget, gpointer user_data)
+void on_window_destroy(GtkWidget * widget, gpointer user_data)
 {
 	quit();
 }
 
-int main(int argc, char **argv) 
-{ 
-	GError *error = NULL; 
-	GtkBuilder *builder = NULL; 
+int main(int argc, char **argv)
+{
+	GError *error = NULL;
+	GtkBuilder *builder = NULL;
 	gchar *filename = NULL;
 
-	GtkWidget *main_window = NULL; 
+	GtkWidget *main_window = NULL;
 	GtkWidget *led_matrix = NULL;
 	GtkWidget *start_button = NULL;
 	GtkWidget *pause_button = NULL;
-	
+
 
 	/* Gtk init */
-	gtk_init(&argc, &argv); 
+	gtk_init(&argc, &argv);
 
-	/* open glade file */ 
-	builder = gtk_builder_new(); 
-	filename =  g_build_filename(GLADE_FILE, NULL); 
+	/* open glade file */
+	builder = gtk_builder_new();
+	filename = g_build_filename(GLADE_FILE, NULL);
 
 	/* load glade file */
-	gtk_builder_add_from_file(builder, filename, &error); 
-	g_free(filename); 
+	gtk_builder_add_from_file(builder, filename, &error);
+	g_free(filename);
 
 	if (error) {
-		gint code = error->code; 
-		g_printerr("%s\n", error->message); 
-		g_error_free(error); 
+		gint code = error->code;
+		g_printerr("%s\n", error->message);
+		g_error_free(error);
 
-		return code; 
+		return code;
 	}
 
 	/* get widgets */
-	main_window = GTK_WIDGET(gtk_builder_get_object (builder, "main_window")); 
-	led_matrix = GTK_WIDGET(gtk_builder_get_object (builder, "led_matrix"));
+	main_window = GTK_WIDGET(gtk_builder_get_object(builder, "main_window"));
+	led_matrix = GTK_WIDGET(gtk_builder_get_object(builder, "led_matrix"));
 
-	start_button = GTK_WIDGET(gtk_builder_get_object (builder, "start_button")); 
-	pause_button = GTK_WIDGET(gtk_builder_get_object (builder, "pause_button")); 
+	start_button = GTK_WIDGET(gtk_builder_get_object(builder, "start_button"));
+	pause_button = GTK_WIDGET(gtk_builder_get_object(builder, "pause_button"));
 
 	/* connect signals */
-    gtk_builder_connect_signals(builder, NULL);
+	gtk_builder_connect_signals(builder, NULL);
 
-    struct _data data;
-    data.pause_button = pause_button;
-    data.start_button = start_button;
-    data.grid = led_matrix;
+	struct _data data;
+	data.pause_button = pause_button;
+	data.start_button = start_button;
+	data.grid = led_matrix;
 
 	g_signal_connect(start_button, "clicked", G_CALLBACK(on_launch_clicked), led_matrix);
 	g_signal_connect(pause_button, "clicked", G_CALLBACK(on_pause_clicked), led_matrix);
 
 	/* display main_window */
-	gtk_widget_show_all(main_window); 
+	gtk_widget_show_all(main_window);
 
-	gtk_main(); 
+	gtk_main();
 
-	return 0; 
-} 
+	return 0;
+}
