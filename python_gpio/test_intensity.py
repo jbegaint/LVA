2d6d40d81a6652550040d658e83d712c9b1a7680
Jeanbroid <jeanbroid@gmail.com>
2014-02-18 11:12:08 +0100

    takes levels from matrix

--- python_gpio/test_intensity.py
+++ python_gpio/test_intensity.py
@@ -2,9 +2,12 @@
 
 import Adafruit_BBIO.GPIO as GPIO
 from time import sleep
+from random import randint
 
 PINS = ["P9_11", "P9_12", "P9_13", "P9_14"]
 T = 1.42
+LEVELS = [0, 1, 2, 3]
+FREQS = [0, T/16, T/8, T]
 
 def setup_pin(pin):
 	GPIO.setup(pin, GPIO.OUT)
@@ -13,27 +16,32 @@ def set_pin_on(pin):
 	GPIO.output(pin, GPIO.LOW)
 
 def set_pin_off(pin):
+        #print("%s off" % pin)
 	GPIO.output(pin, GPIO.HIGH)
 
 def msleep(sec):
+        #print("sleep %s" % sec)
 	sleep(sec/1000.0)
 
 [setup_pin(pin) for pin in PINS ]
 [set_pin_off(pin) for pin in PINS ]
 
-print("starting loop")
+def set_line():
+    for freq in FREQS:
+        for pin in PINS:
+            if (LEVELS[PINS.index(pin)] <= FREQS.index(freq)):
+                set_pin_off(pin)
+        msleep(freq)
 
+c = 0
 while 1:
-	for i in range(0, 3):
-		set_pin_on(PINS[i])
+    [set_pin_on(p) for p in PINS if LEVELS[PINS.index(p)] > 0]
+    set_line()
+    
+    msleep(20 - sum(LEVELS))
 
-	msleep(T/16)
-	set_pin_off(PINS[2])
+    if (c == 50):
+        c = 0
+        #LEVELS = [randint(0, 3) for i in range(0, 4)]
 
-        msleep(T/4)
-	set_pin_off(PINS[1])
-
-	msleep(T)
-	set_pin_off(PINS[0])
-
-        msleep(20 - T/4 - T/16 -T)
+    c += 1
