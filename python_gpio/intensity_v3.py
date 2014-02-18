551d25f3e82d8ba61d5616815ed603532aabb3d1
Jean <jean.begaint@gmail.com>
2014-02-18 20:13:50 +0100

    fix dict madness

--- python_gpio/intensity_v3.py
+++ python_gpio/intensity_v3.py
@@ -2,25 +2,32 @@
 
 from utils import *
 
+# global variables 
 T = 1.42
 SWITCH = "P9_15"
 PINS = ["P9_11", "P9_12", "P9_13", "P9_14"]
-LEVELS = [0, 1, 2, 3]
-PERIODS = [0, T/16, T/8, T]
+LEVELS = [2, 1, 0, 3]
 
+# level -> period
+dict_level_to_period = {
+	0: 0,
+	1: T/16,
+	2: T/8,
+	3: T
+}
+
+# associate level to pin
 dict_pins_levels = dict(zip(PINS, LEVELS))
-dict_levels_periods = dict(zip(LEVELS, PERIODS))
-
 
 def get_dict_pins_periods():
 	d = dict()
-	for pin in PINS:
-		d[pin] = dict_levels_periods[dict_pins_levels[pin]]
 
+	for pin in PINS:
+		d[pin] = dict_level_to_period[dict_pins_levels[pin]]
+	print(d)
 	return d
 
 def main():
-
 	# init board
 	init_pin(SWITCH)
 	set_pin_on(SWITCH)
@@ -33,22 +40,21 @@ def main():
 		[set_pin_on(p) for p in PINS]
 
 		for i in range(0, 17):
-
 			[set_pin_off(p) for p in PINS if (mat[p] == i*T/16)]
-
 			msleep(T/16)
 
 		msleep(20 - T)
 
-try:
-	print("Starting...")
-	main()
+if __name__ == '__main__':
+	try:
+		print("Starting...")
+		main()
 
-except KeyboardInterrupt:
-	print("Exiting...")
-	
-	# switch off board
-	set_pin_off(SWITCH)
+	except KeyboardInterrupt:
+		print("Exiting...")
+		
+		# switch off board
+		set_pin_off(SWITCH)
+		GPIO.cleanup()
 
-	GPIO.cleanup()
-	exit(0)
+		exit(0)
