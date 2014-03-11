698fe018c694d54a527e7ed27184b9cf4e92b18e
Jean <jean.begaint@gmail.com>
2014-03-11 17:37:37 +0100

    fix table, mod tests

--- c_gpio/tests.c
+++ c_gpio/tests.c
@@ -6,11 +6,13 @@
 int main()
 {
 	int i;
-	int n_pins = 3;
+	int n_pins = 4;
 
-	const char *pins_names[3] = {"P9_11", "P9_12", "P9_28"};
+	const char *pins_names[4] = {"P8_11", "P8_12", "P8_15", "P8_16"};
 	pin_t *pins = get_pins_by_names(pins_names, n_pins);
 
+
+	printf("NAME  GPIO PIN\n");
 	for (i = 0; i < n_pins; ++i) {
 		print_pin(pins+i);
 	}
