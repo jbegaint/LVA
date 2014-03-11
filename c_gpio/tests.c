af286cffab94f8cd53b2b351507b75ad4c030497
Jean <jean.begaint@gmail.com>
2014-03-12 00:07:35 +0100

    style

--- c_gpio/tests.c
+++ c_gpio/tests.c
@@ -8,7 +8,7 @@ int main()
 {
 	int n_pins = 4;
 
-	const char *pins_names[4] = {"P8_11", "P8_12", "P8_15", "P8_16"};
+	const char *pins_names[4] = { "P8_11", "P8_12", "P8_15", "P8_16" };
 	pin_t *pins = get_pins_by_names(pins_names, n_pins);
 
 	print_pins(&pins, n_pins);
