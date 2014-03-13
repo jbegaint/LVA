38213f405c53f20e56824fb2c7aa61bc40519a7f
florianayel <florian.ayel@gmail.com>
2014-03-13 15:32:13 +0100

    openNI-2.0 and script bash draft

---
+++ OpenNI/include/PgmToLED.h
@@ -0,0 +1,6 @@
+#ifndef _PGM_TO_LED_H_
+#define _PGM_TO_LED_H_
+
+int PgmToLED(int argc, char** argv);
+
+#endif
---
+++ OpenNI/include/traitement.h
@@ -0,0 +1,11 @@
+#ifndef __TRAITEMENT_H__
+#define __TRAITEMENT_H__
+
+void writePGM_5x7(const char *filename, int **data);
+int** centrage( int** depth , int X , int Y);
+int ** MedianDepth( int** depth );
+float Moyenne ( int** depth, int _x, int _y , int dx, int dy);
+void RempliMatriceReelle(int **Mat,FILE *pf);
+int** seuillage(int** depth , int X , int Y);
+
+#endif
